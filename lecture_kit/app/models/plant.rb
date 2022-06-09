591818cd61e63d2559e536b8756e123b7cb62a48
Donte Ladatto <donte279@gmail.com>
2022-06-09 13:12:58 -0500

    commit tree e2c40edd07b11cb5429c98d98cc9979829e77cfa

--- lecture_kit/app/models/plant.rb
+++ lecture_kit/app/models/plant.rb
@@ -1,5 +1,15 @@
 class Plant < ActiveRecord::Base
     has_many :plant_parenthoods
-    has_many :plant_parents, through: :plant_parenthoods
-    belongs_to :category
+    has_many :people, through: :plant_parenthoods
+    has_many :plant_categories
+    has_many :categories, through: :plant_categories
+    has_many :waterings
+    has_many :waterers, through: :waterings
+
+    def number_of_days_since_last_watering
+        today = DateTime.now
+        last = self.waterings.last.created_at.to_datetime
+        num = (today - last).to_i
+        num == 0 ? "I was watered today!" : "I was watered #{num} days ago."
+    end
 end
---
+++ lecture_kit/db/migrate/20220609165934_change_plant_parent_to_people.rb
@@ -0,0 +1,5 @@
+class ChangePlantParentToPeople < ActiveRecord::Migration[5.2]
+  def change
+    rename_table :plant_parents, :people
+  end
+end
